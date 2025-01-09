c606920ce698067f49a2b927729a4bf531107db2
moose23967 <moose23967@gmail.com>
2025-01-09 15:56:37 +0100

    first commit

---
+++ src/main/kotlin/ru/moose23967/obrikosovo/Obrikosovo.kt
@@ -0,0 +1,14 @@
+package ru.moose23967.obrikosovo
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Obrikosovo : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
